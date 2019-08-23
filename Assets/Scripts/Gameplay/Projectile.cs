09509e05171739eca6f7d0837b9248e64a01cbd7
Cocaine <kokotkin.alexander@gmail.com>
2019-08-23 19:00:21 +0300

    rename

--- Assets/Scripts/Gameplay/Projectile.cs
+++ Assets/Scripts/Gameplay/Projectile.cs
@@ -6,13 +6,10 @@ public class Projectile : MonoBehaviour {
 
     #region Fields
 
-    private Timer boltDeathTimer;
+    private Timer projectileDeathTimer;
     private Rigidbody2D rb;
-    private Animator animator;
 
-    private float direction = 1;
-
-    [SerializeField] private float boltAliveTime = 2f;
+    [SerializeField] private float projectileAliveTime = 2f;
     [SerializeField] private float speed = 5f;
     [SerializeField] private int damage;
     [SerializeField] private GameObject impactEffect;
@@ -30,12 +27,11 @@ public class Projectile : MonoBehaviour {
     private void Start() {
 
         rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
 
-        boltDeathTimer = GetComponent<Timer>();
-        boltDeathTimer.SetTimerName(TimerName.BoltDeathTimer);
-        boltDeathTimer.Duration = boltAliveTime;
-        boltDeathTimer.Run();
+        projectileDeathTimer = GetComponent<Timer>();
+        projectileDeathTimer.SetTimerName(TimerName.ProjectileDeathTimer);
+        projectileDeathTimer.Duration = projectileAliveTime;
+        projectileDeathTimer.Run();
 
         rb.velocity = transform.right * speed;
         
@@ -44,7 +40,7 @@ public class Projectile : MonoBehaviour {
 
     private void Update() {
 
-        if(boltDeathTimer.Finished) {
+        if(projectileDeathTimer.Finished) {
 
             SelfDestroy();
         }

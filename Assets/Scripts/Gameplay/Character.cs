7f9fcab6fb3a96fb2605c21c5add4e6772946449
Cocaine <kokotkin.alexander@gmail.com>
2019-08-26 22:37:09 +0300

    Add Player GUI

--- Assets/Scripts/Gameplay/Character.cs
+++ Assets/Scripts/Gameplay/Character.cs
@@ -7,6 +7,8 @@ public abstract class Character : MonoBehaviour {
     [SerializeField] protected Transform atackPoint;
     [SerializeField] protected GameObject corpse;
     [SerializeField] protected float speed = 5f;
+    [SerializeField] protected int lvl;
+    [SerializeField] protected int exp;
     [SerializeField] protected int hp;
 
     protected abstract void Move(float moveX);

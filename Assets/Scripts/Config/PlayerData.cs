2ab612ebed37d9667c7e69179804601ea4d854a3
Alex <35273835+Cocaine4ik@users.noreply.github.com>
2019-09-11 17:48:29 +0300

    Create PlayerData.cs

---
+++ Assets/Scripts/Config/PlayerData.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData {
+
+    #region Fields
+
+    private int level;
+    private int exp;
+    private int expToReachLevel;
+    private int hp;
+    private int mana;
+
+    private float[] postion;
+    #endregion
+
+    #region Constructor
+    ///
+    public PlayerData(GameObject player) {
+
+        if (player != null) {
+
+            Player playerMain = player.GetComponent<Player>();
+            LevelUpCalculator playerExp = player.GetComponent<LevelUpCalculator>();
+
+            level = playerExp.CurrentLevel;
+            exp = playerExp.CurrentExp;
+            expToReachLevel = playerExp.ExpToReachLevel;
+
+            hp = playerMain.Hp;
+            
+
+            postion = new float[3];
+            postion[0] = player.transform.position.x;
+            postion[1] = player.transform.position.y;
+            postion[2] = player.transform.position.z;
+        }
+
+        else {
+            Debug.Log("Critical Error! Player gameobject is missing!");
+        }
+    }
+
+    #endregion
+}

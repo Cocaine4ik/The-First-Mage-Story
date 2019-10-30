925f640b123fd52695325d0c17b41fc44b2856a1
Alex <35273835+Cocaine4ik@users.noreply.github.com>
2019-10-30 17:00:09 +0200

    Create AtackTrigger.cs

---
+++ Assets/Scripts/Gameplay/AtackTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtackTrigger : MonoBehaviour {
+
+    [SerializeField] protected GameObject owner;
+    [SerializeField] protected int damage;
+
+    public GameObject Owner {
+        get { return owner; }
+    }
+
+    public int Damage {
+        get { return damage; }
+    }
+}

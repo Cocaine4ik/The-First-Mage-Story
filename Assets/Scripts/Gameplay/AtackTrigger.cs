644cd364816a3f13476188373b2108d1af30e345
Alex <35273835+Cocaine4ik@users.noreply.github.com>
2019-12-05 10:23:36 +0200

    delete owners

--- Assets/Scripts/Gameplay/AtackTrigger.cs
+++ Assets/Scripts/Gameplay/AtackTrigger.cs
@@ -4,12 +4,7 @@ using UnityEngine;
 
 public class AtackTrigger : MonoBehaviour {
 
-    [SerializeField] protected GameObject owner;
     [SerializeField] protected int damage;
-
-    public GameObject Owner {
-        get { return owner; }
-    }
 
     public int Damage {
         get { return damage; }

76561fe35d450ae4fd5c7840e94ca2e50aac2cdc
Alex <35273835+Cocaine4ik@users.noreply.github.com>
2019-12-26 17:41:13 +0200

    Create LocalizationData.cs

---
+++ Assets/Scripts/Localization/LocalizationData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class LocalizationData {
+
+    private LocalizationItem[] items;
+
+}
+
+[System.Serializable]
+public class LocalizationItem {
+
+    private string key;
+    private string value;
+ }

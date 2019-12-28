77f9521bb021b6d0f33de11247931c13e2fecd34
Cocaine <kokotkin.alexander@gmail.com>
2019-12-28 15:15:15 +0200

    Create LocalizedTMPro.cs

---
+++ Assets/Scripts/Localization/LocalizedTMPro.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+
+public class LocalizedTMPro : MonoBehaviour {
+
+    public string LocalizationKey;
+
+    public void Start() {
+        Localize();
+        LocalizationManager.LocalizationChanged += Localize;
+    }
+    public void OnDestroy() {
+        LocalizationManager.LocalizationChanged -= Localize;
+    }
+    private void Localize() {
+        GetComponent<TextMeshPro>().text = LocalizationManager.Localize(LocalizationKey);
+    }
+}

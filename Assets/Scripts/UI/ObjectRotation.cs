efa46b390a3f4d0b40ca54e623bcfa0c3bc48c60
Cocaine <kokotkin.alexander@gmail.com>
2020-01-08 10:10:45 +0200

    Create ObjectRotation.cs

---
+++ Assets/Scripts/UI/ObjectRotation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotation : MonoBehaviour
+{
+
+    #region Fields
+
+    [SerializeField] private float speed = 45;
+    RectTransform rectTransform;
+
+    #endregion
+
+    #region Methods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rectTransform.Rotate(new Vector3(0, 0, speed) * Time.deltaTime);
+    }
+
+    #endregion
+}

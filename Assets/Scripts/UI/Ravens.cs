0ebbdf31bb7096921a8f13f57a430e652e235c7b
Alex <35273835+Cocaine4ik@users.noreply.github.com>
2020-01-13 17:30:10 +0200

    Create Ravens.cs
    Need Rework

---
+++ Assets/Scripts/UI/Ravens.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ravens : MonoBehaviour
+{
+
+    private BoxCollider2D collider2D;
+
+    private float colliderWidth;
+    private float colliderHeight;
+
+
+    public float ColliderWidth {
+        get { return colliderWidth; }
+    }
+
+    public float ColliderHeight {
+        get { return colliderHeight; }
+    }
+
+    #region Methods
+
+    private void Start() {
+
+        collider2D = GetComponent<BoxCollider2D>();
+
+        colliderWidth = collider2D.size.x;
+        colliderHeight = collider2D.size.y;
+    }
+    private void Update() {
+
+        if (gameObject.transform.position.x + colliderWidth  < ScreenUtils.ScreenLeft) {
+            EventManager.TriggerEvent(EventName.SpawnRavens, new EventArg());
+            Destroy(gameObject);
+        }
+
+    }
+
+    #endregion
+}

7e1759f2070c932b21ca92ae369f81dc4bfa70c2
Alex <35273835+Cocaine4ik@users.noreply.github.com>
2020-01-15 17:41:17 +0200

    Delete old colider propeties

--- Assets/Scripts/UI/Ravens.cs
+++ Assets/Scripts/UI/Ravens.cs
@@ -7,34 +7,21 @@ public class Ravens : MonoBehaviour
 
     private BoxCollider2D collider2D;
 
-    private float colliderWidth;
-    private float colliderHeight;
-
-
-    public float ColliderWidth {
-        get { return colliderWidth; }
-    }
-
-    public float ColliderHeight {
-        get { return colliderHeight; }
-    }
 
     #region Methods
 
     private void Start() {
 
         collider2D = GetComponent<BoxCollider2D>();
-
-        colliderWidth = collider2D.size.x;
-        colliderHeight = collider2D.size.y;
     }
     private void Update() {
 
-        if (gameObject.transform.position.x + colliderWidth  < ScreenUtils.ScreenLeft) {
+        /*
+        if (gameObject.transform.position.x + collider2D.size.x;  < ScreenUtils.ScreenLeft) {
             EventManager.TriggerEvent(EventName.SpawnRavens, new EventArg());
             Destroy(gameObject);
         }
-
+        */
     }
 
     #endregion

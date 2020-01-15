ce5f1aff233e99b803343a0a90a39d22c2097347
Alex <35273835+Cocaine4ik@users.noreply.github.com>
2020-01-15 17:41:04 +0200

    Fix colider width problems, rewrite first spawn

--- Assets/Scripts/UI/RavensSpawner.cs
+++ Assets/Scripts/UI/RavensSpawner.cs
@@ -4,16 +4,35 @@ using UnityEngine;
 
 public class RavensSpawner : MonoBehaviour
 {
+    #region Fields
+
     [SerializeField] private GameObject ravensPrefab;
     [SerializeField] private GameObject canvas;
-    private Ravens ravens;
 
+    // ravnesPrefab colider component
+    private BoxCollider2D boxCollider2D;
+
+    private float ravensPrefabWidth;
+    private float ravensPrefabHeight;
+
+    #endregion
+
+    #region Methods
 
     // Start is called before the first frame update
     void Start()
     {
         EventManager.StartListening(EventName.SpawnRavens, OnSpawnRavens);
-        ravens = ravensPrefab.GetComponent<Ravens>();
+
+        GameObject tempRavens = Instantiate(ravensPrefab);
+
+        boxCollider2D = tempRavens.GetComponent<BoxCollider2D>();
+
+        ravensPrefabWidth = boxCollider2D.size.x;
+        ravensPrefabHeight = boxCollider2D.size.y;
+
+        Destroy(tempRavens);
+
         SpawnRavens();
 
     }
@@ -28,21 +47,26 @@ public class RavensSpawner : MonoBehaviour
     void Update()
     {
 
-        Debug.Log(ravens.ColliderWidth);
     }
 
     private void SpawnRavens() {
 
-        Debug.Log(ravens.ColliderWidth);
-        GameObject birds = Instantiate(ravensPrefab, canvas.transform);
-        Vector2 spawnPos = new Vector2(Screen.width/2 + ravens.ColliderWidth, -Screen.height/2);
-        birds.transform.localPosition = spawnPos;
+        Debug.Log(ravensPrefabWidth);
+
+        GameObject ravens = Instantiate(ravensPrefab, canvas.transform);
+        Vector2 spawnPos = new Vector2(Screen.width/2 + ravensPrefabWidth , -Screen.height/2);
+        ravens.transform.localPosition = spawnPos;
     }
+
+    #endregion
+
+    #region Events
 
     public void OnSpawnRavens(EventArg arg) {
 
         SpawnRavens();
 
     }
-    
+
+    #endregion
 }

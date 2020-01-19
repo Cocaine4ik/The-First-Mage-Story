0f8635ddb0f73cb39062dc4f456e72f7e459d258
Cocaine <kokotkin.alexander@gmail.com>
2020-01-19 21:48:58 +0200

    Create Item.cs

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Unlike base class Item have a price
+/// </summary>
+[CreateAssetMenu(menuName = "Items/Standard Item")]
+public class Item : ItemBase
+{
+
+}

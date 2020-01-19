76f55e77954bce2d6f808b43d19f28eee70171af
Cocaine <kokotkin.alexander@gmail.com>
2020-01-19 21:48:36 +0200

    Create IITem.cs
    Iterface for Base Item

---
+++ Assets/Scripts/Items/IITem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IITem {
+
+    string ItemName { get; }
+    Sprite ItemIcon { get; }
+    string ItemDescription { get;}
+
+    /// <summary>
+    /// keys for localiztion
+    /// </summary>
+    string ItemNameKey { get;  }
+    string ItemDescriptionKey { get; }
+
+}

a0e10bab4e7c71c34f1914c5ac1df7f228c7429d
Cocaine <kokotkin.alexander@gmail.com>
2020-01-19 21:48:45 +0200

    Create Inventory.cs
    test

---
+++ Assets/Scripts/Items/Inventory.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Inventory : MonoBehaviour
+{
+
+    #region Fields
+    private int inventorySize;
+    [SerializeField] private RelicItem test;
+    [SerializeField] private Item testItem;
+    /// <summary>
+    /// 
+    /// </summary>
+    private List<Item> items;
+    /// <summary>
+    /// 
+    /// </summary>
+    private List<Item> relics;
+    #endregion
+
+    #region Methods
+
+    public void AddItem(Item item) {
+
+        if(items.Count < inventorySize) {
+            items.Add(item);
+        }
+        else {
+            Debug.Log("Inventory is full");
+        }
+    }
+
+    private void Start() {
+
+        items = new List<Item>();
+        //items.Add(testItem);
+        items.Add(test);
+        if (items[0].GetType() == typeof(RelicItem)) {
+            Debug.Log("it's ok");
+        }
+    }
+
+    #endregion
+}

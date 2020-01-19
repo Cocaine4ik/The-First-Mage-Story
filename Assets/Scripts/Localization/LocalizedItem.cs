1d6c92bbec330cb9cadaf89271e83f96acc85732
Cocaine <kokotkin.alexander@gmail.com>
2020-01-19 21:49:25 +0200

    Create LocalizedItem.cs

---
+++ Assets/Scripts/Localization/LocalizedItem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class LocalizedItem : MonoBehaviour
+{
+    public string LocalizationKeyName;
+    public string LocalizationKeyDescription;
+
+    public void Start() {
+        Localize();
+        LocalizationManager.LocalizationChanged += Localize;
+    }
+
+    public void OnDestroy() {
+        LocalizationManager.LocalizationChanged -= Localize;
+    }
+
+    private void Localize() {/*
+        GetComponent<Item>().LocalizeItemName(LocalizationManager.Localize(LocalizationKeyName));
+        GetComponent<Item>().LocalizeItemDescription(LocalizationManager.Localize(LocalizationKeyDescription));*/
+    }
+}

e98134dabfe74a7157b03653c4c0bb7c740f1566
Cocaine <kokotkin.alexander@gmail.com>
2020-01-25 14:25:29 +0200

    Create PlayerInterfaceController.cs

---
+++ Assets/Scripts/UI/PlayerInterfaceController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInterfaceController : MonoBehaviour
+{
+    [SerializeField] private GameObject inventory;
+
+    private void Update() {
+
+        if (Input.GetKeyDown(KeyCode.I)) {
+            inventory.SetActive(!inventory.gameObject.activeSelf);
+        }
+
+    }
+}

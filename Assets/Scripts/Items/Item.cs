d5d1b263903635d95c2ced1be89bb6215c9259ac
Cocaine <kokotkin.alexander@gmail.com>
2020-01-26 21:55:02 +0200

    add ItemType properties

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -6,28 +6,24 @@ public class Item : ScriptableObject, IITem {
     #region Fields
 
     [Header("Item parameters")]
-    [SerializeField] protected string itemName;
-    [SerializeField] protected string itemType;
     [SerializeField] protected Sprite itemIcon;
-    [SerializeField] protected string itemDescription;
 
     [Header("Localiztion keys")]
     [SerializeField] protected string itemNameKey;
-    [SerializeField] protected string itemTypeKey;
     [SerializeField] protected string itemDescriptionKey;
+
+    protected string itemTypeKey;
+    protected string itemTypeKeyDefault = "Items.Type.";
 
     #endregion
 
     #region Properties
 
-    public string ItemName => itemName;
-    public string ItemType => itemType;
     public Sprite ItemIcon => itemIcon;
-    public string ItemDescription => itemDescription;
 
     public string ItemNameKey => itemNameKey;
-    public string ItemTypeKey => itemTypeKey;
     public string ItemDescriptionKey => itemDescriptionKey;
+    public string ItemTypeKey => itemTypeKeyDefault + GetType().ToString();
 
     #endregion
 }

0b6420662925e576432f417c9cd0e5ee24e7b2fa
Cocaine <kokotkin.alexander@gmail.com>
2020-01-26 21:23:25 +0200

    Update Item.cs

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -1,12 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
-/// <summary>
-/// Unlike base class Item have a price
-/// </summary>
-[CreateAssetMenu(menuName = "Items/Standard Item")]
-public class Item : ItemBase
-{
+public class Item : ScriptableObject, IITem {
 
+    #region Fields
+
+    [Header("Item parameters")]
+    [SerializeField] protected string itemName;
+    [SerializeField] protected string itemType;
+    [SerializeField] protected Sprite itemIcon;
+    [SerializeField] protected string itemDescription;
+
+    [Header("Localiztion keys")]
+    [SerializeField] protected string itemNameKey;
+    [SerializeField] protected string itemTypeKey;
+    [SerializeField] protected string itemDescriptionKey;
+
+    #endregion
+
+    #region Properties
+
+    public string ItemName => itemName;
+    public string ItemType => itemType;
+    public Sprite ItemIcon => itemIcon;
+    public string ItemDescription => itemDescription;
+
+    public string ItemNameKey => itemNameKey;
+    public string ItemTypeKey => itemTypeKey;
+    public string ItemDescriptionKey => itemDescriptionKey;
+
+    #endregion
 }

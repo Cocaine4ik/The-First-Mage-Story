e296695c0ab08044e050a9a44fec6fe1f7ae99f5
Cocaine <kokotkin.alexander@gmail.com>
2020-01-26 21:23:08 +0200

    Update InventoryCell.cs

--- Assets/Scripts/Items/InventoryCell.cs
+++ Assets/Scripts/Items/InventoryCell.cs
@@ -6,15 +6,18 @@ using UnityEngine.UI;
 public class InventoryCell : MonoBehaviour
 {
     [SerializeField] private Image iconField;
+    private string itemNameKey;
+    private string itemTypeKey;
     [SerializeField] private int id;
     [SerializeField] private bool isEmpty;
-
+    [SerializeField] private bool isFull;
 
     public bool IsEmpty => isEmpty;
 
     private void Awake() {
 
         isEmpty = true;
+        isFull = false;
     }
 
     public void SetId(int id) {
@@ -22,9 +25,19 @@ public class InventoryCell : MonoBehaviour
         this.id = id;
     }
 
-    public void Render(IITem item) {
+    public void AddItemToCell(IITem item) {
 
         iconField.sprite = item.ItemIcon;
+        itemNameKey = item.ItemNameKey;
+        isFull = true;
     }
-   
+
+    public void OnInventoryCellSelected() {
+
+        if (isFull == true) {
+            EventManager.TriggerEvent(EventName.ShowInventoryName, new EventArg(itemNameKey));
+        }
+
+    }
+
 }

ed431278966c34411db18d909f9ee82c835a2ff1
Cocaine <kokotkin.alexander@gmail.com>
2020-01-26 21:23:11 +0200

    Create InventoryItemName.cs

---
+++ Assets/Scripts/Items/InventoryItemName.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryItemName : MonoBehaviour
+{
+    private void Start() {
+
+        EventManager.StartListening(EventName.ShowInventoryName, OnChangedNameKey);
+    }
+
+    private void OnDestroy() {
+
+        EventManager.StopListening(EventName.ShowInventoryName, OnChangedNameKey);
+
+    }
+    public void OnChangedNameKey(EventArg arg) {
+
+        GetComponent<LocalizedTMPro>().ChangeLocalizationKey(arg.FirstStringArg);
+        GetComponent<LocalizedTMPro>().ChangeLocalization();
+    }
+}

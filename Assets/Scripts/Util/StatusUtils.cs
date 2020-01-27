76538c6ba1a39741c835defb49ca13478ec15322
Cocaine <kokotkin.alexander@gmail.com>
2020-01-27 21:22:40 +0200

    Update StatusUtils.cs

--- Assets/Scripts/Util/StatusUtils.cs
+++ Assets/Scripts/Util/StatusUtils.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class StatusUtils
 {
+    #region Fields
+
+    private static bool inventoryClosed = true;
+
+    #endregion
+
+    #region Properties
 
     public static bool IsPause { get; set; }
 
     public static bool MusicOn { get; set; }
+
+    public static bool InventoryClosed { get; set; }
+
+    #endregion
 }

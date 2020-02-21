a8253429c54c49b4e05c3e7d940f5bd92fc25b4f
Alex <35273835+Cocaine4ik@users.noreply.github.com>
2020-02-21 16:41:10 +0200

    Update RangeCharacter.cs

--- Assets/Scripts/Gameplay/Characters/Base/RangeCharacter.cs
+++ Assets/Scripts/Gameplay/Characters/Base/RangeCharacter.cs
@@ -6,7 +6,7 @@ public class RangeCharacter : Character {
 
     [SerializeField] protected GameObject projectilePrefab;
 
-    protected void OnRangeAtack() {
+        protected void OnRangeAtack() {
 
         Instantiate(projectilePrefab, new Vector3(atackPoint.position.x, atackPoint.position.y,
             atackPoint.position.z), atackPoint.rotation);

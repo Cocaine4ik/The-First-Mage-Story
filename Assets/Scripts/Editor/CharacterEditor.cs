870e82c478cb0100bcf54997ab32dd901f9da4ab
Alex <35273835+Cocaine4ik@users.noreply.github.com>
2020-03-03 13:07:02 +0200

    Create CharacterEditor.cs

---
+++ Assets/Scripts/Editor/CharacterEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEditor.AnimatedValues;
+
+[CustomEditor(typeof(MonoBehaviour))]
+public class CharacterEditor : Editor {
+
+    AnimBool jumpSettings;
+    Character character;
+
+    private void OnEnable() {
+
+        character = (Character)target;
+        jumpSettings = new AnimBool(false);
+    }
+
+}

bf0783fe1342b78879accb171afb1ab8634f7f6f
Alex <35273835+Cocaine4ik@users.noreply.github.com>
2020-03-04 16:35:33 +0200

    Add Foldout for jump setting in Character class

--- Assets/Scripts/Editor/CharacterEditor.cs
+++ Assets/Scripts/Editor/CharacterEditor.cs
@@ -1,17 +1,27 @@
 ﻿using UnityEditor;
-using UnityEngine;
-using UnityEditor.AnimatedValues;
 
-[CustomEditor(typeof(MonoBehaviour))]
-public class CharacterEditor : Editor {
+[CustomEditor(typeof(Character))]
+ public class CharacterEditor : Editor {
 
-    AnimBool jumpSettings;
-    Character character;
+    protected static bool showJumpSetting = false; //declare outside of function
+    private Character character;
 
     private void OnEnable() {
 
-        character = (Character)target;
-        jumpSettings = new AnimBool(false);
+        character = target as Character;
     }
 
+    public override void OnInspectorGUI() {
+
+        base.OnInspectorGUI();
+        showJumpSetting = EditorGUILayout.Foldout(showJumpSetting, "Jump Setting");
+        
+        if (showJumpSetting) {
+
+            character.CheckRadious = EditorGUILayout.FloatField("Jump Checking Radius", character.CheckRadious);
+            character.JumpForce = EditorGUILayout.FloatField("Jump Force", character.JumpForce);
+            character.JumpControlTime = EditorGUILayout.FloatField("Jump Control Time", character.JumpControlTime);
+
+        }
+    }
 }

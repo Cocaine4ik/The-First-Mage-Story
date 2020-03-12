449d6c45624946600772235535a6a4111aee2c16
Alex <35273835+Cocaine4ik@users.noreply.github.com>
2020-03-12 16:57:30 +0200

    Create LocalizedTMProMenu.cs

---
+++ Assets/Scripts/Localization/LocalizedTMProMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class LocalizedTMProMenu : LocalizedTMPro
+{
+
+    protected override void Localize() {
+
+        // change font asset before localize if it's Menu and language is Russian
+        if (LocalizationManager.Language == SystemLanguage.Russian.ToString()) {
+            text.font = Resources.Load<TMP_FontAsset>("Fonts/Silver SDF Menu");
+        }
+
+        GetComponent<TextMeshProUGUI>().text = LocalizationManager.Localize(localizationKey);
+    }
+
+}

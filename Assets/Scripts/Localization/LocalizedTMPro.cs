f68ce799b481ca78ebfa1ec401cf084e66e3f320
Alex <35273835+Cocaine4ik@users.noreply.github.com>
2020-03-12 16:57:33 +0200

    Update LocalizedTMPro.cs

--- Assets/Scripts/Localization/LocalizedTMPro.cs
+++ Assets/Scripts/Localization/LocalizedTMPro.cs
@@ -1,32 +1,41 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
 [RequireComponent(typeof(TextMeshProUGUI))]
 
 public class LocalizedTMPro : MonoBehaviour {
 
-    [SerializeField] private string localizationKey;
+    [SerializeField] protected string localizationKey;
+    protected TextMeshProUGUI text;
 
     public void Start() {
 
+        text = GetComponent<TextMeshProUGUI>();
         ChangeLocalization();
 
     }
     public void OnDestroy() {
         LocalizationManager.LocalizationChanged -= Localize;
     }
-    private void Localize() {
+
+    protected virtual void Localize() {
+
         GetComponent<TextMeshProUGUI>().text = LocalizationManager.Localize(localizationKey);
     }
 
+    /// <summary>
+    ///  Change localization
+    /// </summary>
     public void ChangeLocalization() {
 
         Localize();
         LocalizationManager.LocalizationChanged += Localize;
 
     }
-
+    /// <summary>
+    /// Change localization key for inventory item changes
+    /// </summary>
+    /// <param name="key"></param>
     public void ChangeLocalizationKey(string key) {
         localizationKey = key;
     }

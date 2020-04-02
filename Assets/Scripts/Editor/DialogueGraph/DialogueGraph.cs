03dae3293d222c9fc3c698084264120bea1aaa76
Cocaine <kokotkin.alexander@gmail.com>
2020-04-02 13:59:59 +0300

    Update DialogueGraph.cs

--- Assets/Scripts/Editor/DialogueGraph/DialogueGraph.cs
+++ Assets/Scripts/Editor/DialogueGraph/DialogueGraph.cs
@@ -119,6 +119,7 @@ public class DialogueGraph : EditorWindow
             }
             else {
                 saveUtility.LoadGraph(fileName);
+                
             }
         }
         else {

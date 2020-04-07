6e804d227ec3efed9a90685b25f33b7c54780f0f
Cocaine <kokotkin.alexander@gmail.com>
2020-04-07 14:05:10 +0300

    Create DialogueSpeaker.cs

---
+++ Assets/Scripts/Dialogues/DialogueSpeaker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Dialogue speaker class
+/// icnludes getters for dialogue portraits and speakers name
+/// + speaker names localization
+/// </summary>
+/// 
+public class DialogueSpeaker : MonoBehaviour
+{
+    [SerializeField] private string speakerLocalizationKey;
+    [SerializeField] private Image speakerPortrait;
+
+    private string dialogueSpeakerName;
+
+    public string DialogueSpeakerName => dialogueSpeakerName;
+    public Image SpeakerPortait => speakerPortrait; 
+
+    private void Start() {
+
+        dialogueSpeakerName = LocalizationManager.Localize(speakerLocalizationKey);
+
+    }
+}

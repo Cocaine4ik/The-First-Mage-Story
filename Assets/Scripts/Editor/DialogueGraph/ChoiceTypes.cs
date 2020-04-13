a2c7dbaeb9074e11957b1faa508ec20fc7727313
Cocaine <kokotkin.alexander@gmail.com>
2020-04-13 10:45:40 +0300

    Create ChoiceTypes.cs

---
+++ Assets/Scripts/Editor/DialogueGraph/ChoiceTypes.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ChoiceTypes : PopupWindowContent {
+
+    public override void OnGUI(Rect rect) {
+        throw new System.NotImplementedException();
+    }
+}

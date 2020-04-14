cc5710534ac0694011a3780cf03a8d9e66f665b4
Cocaine <kokotkin.alexander@gmail.com>
2020-04-14 14:18:43 +0300

    add Dialoge Event Invoker

--- Assets/Scripts/Dialogues/DialogueTrigger.cs
+++ Assets/Scripts/Dialogues/DialogueTrigger.cs
@@ -8,7 +8,9 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     [SerializeField] private DialogueContainer dialogue;
+
     private Transform dialogueWindow;
+    private bool dialogueSet = false;
 
     /// <summary>
     /// Start conversation with trigger with out interact from player
@@ -20,10 +22,32 @@ public class DialogueTrigger : MonoBehaviour
             StartConversation();
         }
     }
+    /// <summary>
+    /// If dialogue trigger is interactable we setdialogue and invoke readyToInteract event withtrue value
+    /// </summary>
+    /// <param name="collision"></param>
     private void OnTriggerStay2D(Collider2D collision) {
-               
-    }
 
+        if (GetComponent<InteractableObject>() != null && collision.GetComponent<Player>() != null) {
+
+            if(dialogueWindow != null && !dialogueSet) {
+                dialogueWindow.GetComponent<DialogueParser>().SetDialogue(dialogue);
+                dialogueSet = true;
+                Debug.Log("Dialogue set: " + dialogue.name);
+            }
+            EventManager.TriggerEvent(EventName.ReadyToInteract, new EventArg(true));
+        }
+    }
+    /// <summary>
+    /// If player exit dialogue trigger we invoke readyToInteract event with false value
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnTriggerExit2D(Collider2D collision) {
+
+        if (GetComponent<InteractableObject>() != null && collision.GetComponent<Player>() != null) {
+            EventManager.TriggerEvent(EventName.ReadyToInteract, new EventArg(false));
+        }
+    }
     /// <summary>
     /// Initialize dialogue window
     /// </summary>
@@ -39,6 +63,7 @@ public class DialogueTrigger : MonoBehaviour
     private void StartConversation() {
 
         dialogueWindow.GetComponent<DialogueParser>().SetDialogue(dialogue);
+        dialogueSet = true;
         Debug.Log("Dialogue set: " + dialogue.name);
         EventManager.TriggerEvent(EventName.StartConversation);
         Debug.Log("Starting conversation.");

879b169c46c022f231715111e7cbbbe2558c1c3d
Cocaine <kokotkin.alexander@gmail.com>
2020-05-13 11:02:29 +0300

    Create TalkTrigger.cs

---
+++ Assets/Scripts/Gameplay/Dialogues/TalkTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Abstract dialogue and speech trigger class
+/// </summary>
+public abstract class TalkTrigger : MonoBehaviour {
+
+    [Header("Base Talk Options:")]
+    [SerializeField] protected bool isInteractable = false;
+    [SerializeField] protected bool isScriptable = false;
+    [SerializeField] protected float scriptableTime;
+
+    protected abstract void StartConversation();
+
+    protected abstract IEnumerator StartScriptableConversation(float scriptableTime);
+}

390faa9d41c5f60d75581c59b3ee0eeffcafc3bc
Cocaine <kokotkin.alexander@gmail.com>
2020-05-13 11:05:50 +0300

    Create AudioTrigger.cs

---
+++ Assets/Scripts/Audio/AudioTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTrigger : MonoBehaviour {
+    [SerializeField] private AudioClipName clipName;
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.GetComponent<Player>() != null || collision.gameObject.name == "Archmage") {
+            AudioManager.Play(clipName);
+        }
+    }
+}

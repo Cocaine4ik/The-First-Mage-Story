2db3e7c9ecd54b2cef6ad173fd89d36ab1397ec3
Cocaine <kokotkin.alexander@gmail.com>
2020-06-10 13:10:45 +0300

    add Videop Player for Spell Prview in Spellbook

---
+++ Assets/Scripts/UI/StreamVideo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+
+public class StreamVideo : MonoBehaviour
+{
+    [SerializeField] private RawImage streamWindow;
+    [SerializeField] private VideoPlayer videoPlayer;
+    [SerializeField] private AudioSource audioSource;
+
+    IEnumerator PlayVideo() {
+
+        videoPlayer.Prepare();
+        WaitForSeconds waitForSeconds = new WaitForSeconds(1);
+        while(!videoPlayer.isPrepared) {
+            yield return waitForSeconds;
+            break;
+        }
+        streamWindow.texture = videoPlayer.texture;
+        videoPlayer.Play();
+        audioSource.Play();
+    }
+}

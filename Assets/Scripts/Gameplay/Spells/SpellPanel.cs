839db57651b48addf747d5f8eaab04cf6a93eb27
Cocaine <kokotkin.alexander@gmail.com>
2020-06-17 17:14:18 +0300

    Add Input detection

--- Assets/Scripts/Gameplay/Spells/SpellPanel.cs
+++ Assets/Scripts/Gameplay/Spells/SpellPanel.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SpellPanel : MonoBehaviour
 {
-    private List<GameObject> spells = new List<GameObject>();
+    [SerializeField] private List<SpellInvoker> spellInvokers;
 
     // Start is called before the first frame update
     void Start()
@@ -14,7 +14,37 @@ public class SpellPanel : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {   
+        if (Input.GetButtonDown("[1] Spell Panel Cell")) {
+            spellInvokers[1].InvokeSpell();
+        }
+        if (Input.GetButtonDown("[2] Spell Panel Cell")) {
+            spellInvokers[2].InvokeSpell();
+        }
+        if (Input.GetButtonDown("[3] Spell Panel Cell")) {
+            spellInvokers[3].InvokeSpell();
+        }
+        if (Input.GetButtonDown("[4] Spell Panel Cell")) {
+            spellInvokers[4].InvokeSpell();
+        }
+        if (Input.GetButtonDown("[5] Spell Panel Cell")) {
+            spellInvokers[5].InvokeSpell();
+        }
+        if (Input.GetButtonDown("[6] Spell Panel Cell")) {
+            spellInvokers[6].InvokeSpell();
+        }
+        if (Input.GetButtonDown("[7] Spell Panel Cell")) {
+            spellInvokers[7].InvokeSpell();
+        }
+        if (Input.GetButtonDown("[8] Spell Panel Cell")) {
+            spellInvokers[8].InvokeSpell();
+        }
+        if (Input.GetButtonDown("[9] Spell Panel Cell")) {
+            spellInvokers[9].InvokeSpell();
+        }
+        if (Input.GetButtonDown("[0] Spell Panel Cell")) {
+            spellInvokers[0].InvokeSpell();
+        }
+
     }
 }

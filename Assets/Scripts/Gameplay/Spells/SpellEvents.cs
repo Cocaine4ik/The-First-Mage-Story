baca99b1d2f3182aa17fe3057bc4a66c7eedac70
Cocaine <kokotkin.alexander@gmail.com>
2020-06-17 17:13:49 +0300

    Create SpellEvents.cs

---
+++ Assets/Scripts/Gameplay/Spells/SpellEvents.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellEvents : MonoBehaviour
+{
+
+    private void OnEnable() {
+        EventManager.StartListening(EventName.CastFireball, OnCastFireball);
+        EventManager.StopListening(EventName.CastFrostbolt, OnCastFrostbolt);
+    }
+    private void OnDisable() {
+        EventManager.StopListening(EventName.CastFireball, OnCastFireball);
+        EventManager.StopListening(EventName.CastFrostbolt, OnCastFrostbolt);
+    }
+
+    #region Events
+
+    private void OnCastFireball(EventArg arg) {
+        Debug.Log("Cast: " + arg.Spell.name.ToString());
+    }
+    private void OnCastFrostbolt(EventArg arg) {
+        Debug.Log("Cast: " + arg.Spell.name.ToString());
+    }
+    #endregion
+}

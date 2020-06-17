36f4100f900de67cbfc089b6ea1db2ed4b22f164
Alex <35273835+Cocaine4ik@users.noreply.github.com>
2020-06-17 21:58:07 +0300

    Update SpellPanelCell.cs

--- Assets/Scripts/Gameplay/Spells/SpellPanelCell.cs
+++ Assets/Scripts/Gameplay/Spells/SpellPanelCell.cs
@@ -1,10 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpellPanelCell : MonoBehaviour
 {
     [SerializeField] private int id;
+    private Image image;
+    private SpellInvoker spellInvoker;
 
     public int Id => id;
+    public Image Image => image;
+    public SpellInvoker SpellInvoker => spellInvoker;
+
+    private void Start() {
+        image = GetComponent<Image>();
+        spellInvoker = GetComponent<SpellInvoker>();
+    }
+
 }

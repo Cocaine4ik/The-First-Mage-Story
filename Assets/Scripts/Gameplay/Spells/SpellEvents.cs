0332d7c7ba8f5705e8a2d8852907e0725150f935
Cocaine <kokotkin.alexander@gmail.com>
2020-06-18 17:50:24 +0300

    Update SpellEvents.cs

--- Assets/Scripts/Gameplay/Spells/SpellEvents.cs
+++ Assets/Scripts/Gameplay/Spells/SpellEvents.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SpellEvents : MonoBehaviour
 {
+    private Transform atackTrigger;
 
     private void OnEnable() {
         EventManager.StartListening(EventName.CastFireball, OnCastFireball);
@@ -14,9 +15,22 @@ public class SpellEvents : MonoBehaviour
         EventManager.StopListening(EventName.CastFrostbolt, OnCastFrostbolt);
     }
 
+    private void Start() {
+
+        atackTrigger = GetComponent<Player>().AtackTrigger;
+    }
     #region Events
 
+    private void CastProjectile(GameObject projectilePrefab) {
+
+        GameObject projectile = Instantiate(projectilePrefab, new Vector2(atackTrigger.gameObject.transform.position.x,
+        atackTrigger.gameObject.transform.position.y), atackTrigger.gameObject.transform.rotation);
+
+        projectile.GetComponent<Projectile>().SetOwner(gameObject);
+    }
     private void OnCastFireball(EventArg arg) {
+
+        CastProjectile(arg.Spell.ProjectilePrefab);        
         Debug.Log("Cast: " + arg.Spell.name.ToString());
     }
     private void OnCastFrostbolt(EventArg arg) {

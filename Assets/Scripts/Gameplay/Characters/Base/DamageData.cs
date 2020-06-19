da144b8dfe833051a1554add6769dd385a118c02
Cocaine <kokotkin.alexander@gmail.com>
2020-06-19 17:26:16 +0300

    Update DamageData.cs

--- Assets/Scripts/Gameplay/Characters/Base/DamageData.cs
+++ Assets/Scripts/Gameplay/Characters/Base/DamageData.cs
@@ -6,6 +6,8 @@ public class DamageData : MonoBehaviour {
 
     [SerializeField] protected int damage;
     [SerializeField] protected float atackRange;
+    [SerializeField] private DamageType damageType;
+    [SerializeField] private EffectName effect = EffectName.None;
 
     public float AtackRange => atackRange;
 
@@ -15,7 +17,15 @@ public class DamageData : MonoBehaviour {
                 return damage + Attributes.Instance.Knowledge;
             else return damage;
         }
+        set { damage = value; }
     }
 
-
+    public DamageType  DamageType {
+        get { return damageType; }
+        set { damageType = value; }
+    }
+    public EffectName Effect {
+        get { return effect; }
+        set { effect = value; }
+    }
 }

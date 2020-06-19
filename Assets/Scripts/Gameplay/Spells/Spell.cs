886c91fc72155f8ca03b38763025fb20234bfcf4
Cocaine <kokotkin.alexander@gmail.com>
2020-06-19 17:26:28 +0300

    Update Spell.cs

--- Assets/Scripts/Gameplay/Spells/Spell.cs
+++ Assets/Scripts/Gameplay/Spells/Spell.cs
@@ -11,21 +11,21 @@ public class Spell : ScriptableObject, ISpell
     [SerializeField] private GameObject projectilePrefab;
     [SerializeField] private EventName invokeEvent;
     [SerializeField] private SpellType spellType;
+    [SerializeField] private int spellDamage;
     [SerializeField] private DamageType damageType;
     [SerializeField] private EffectName spellEffect = EffectName.None;
+    [SerializeField] private int manaCost;
+    [SerializeField] private float cooldown;
 
     public SpellName SpellName => spellName;
-
-    public GameObject SpellCastPrefab => throw new System.NotImplementedException();
-
-    public GameObject ProjectilePrefab => throw new System.NotImplementedException();
-
+    public GameObject SpellCastPrefab => spellCastPrefab;
+    public GameObject ProjectilePrefab => projectilePrefab;
     public EventName InvokeEvent => invokeEvent;
+    public SpellType SpellType => spellType;
+    public EffectName SpellEffect => spellEffect;
+    public int SpellDamage => spellDamage;
+    public DamageType DamageType => damageType;
+    public int ManaCost => manaCost;
+    public float Cooldown => cooldown;
 
-    public enum SpellType {
-        Summon,
-        Projectile,
-        Target,
-        Self
-    }
 }

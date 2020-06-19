e06fb0d4287d1dbe7661519286a69d6878f7a543
Cocaine <kokotkin.alexander@gmail.com>
2020-06-19 17:26:24 +0300

    Update ISpell.cs

--- Assets/Scripts/Gameplay/Spells/ISpell.cs
+++ Assets/Scripts/Gameplay/Spells/ISpell.cs
@@ -5,4 +5,10 @@ public interface ISpell
     GameObject SpellCastPrefab { get; }
     GameObject ProjectilePrefab { get; }
     EventName InvokeEvent { get;  }
+    SpellType SpellType { get;  }
+    int SpellDamage { get; }
+    DamageType DamageType { get; }
+    EffectName SpellEffect { get; }
+    int ManaCost { get; }
+    float Cooldown { get; }
 }

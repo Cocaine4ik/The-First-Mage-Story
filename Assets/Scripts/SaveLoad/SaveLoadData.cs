9637c3ed1453e7b6bc3b3da1b4d48195992664ab
Alex <35273835+Cocaine4ik@users.noreply.github.com>
2020-06-25 23:22:41 +0300

    Create SaveLoadData.cs

---
+++ Assets/Scripts/SaveLoad/SaveLoadData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public abstract class SaveLoadData : MonoBehaviour
+{
+    protected void OnEnable() {
+        EventManager.StartListening(EventName.SaveData, OnSaveData);
+        EventManager.StartListening(EventName.LoadData, OnLoadData);
+    }
+    protected void OnDisable() {
+        EventManager.StopListening(EventName.SaveData, OnSaveData);
+        EventManager.StopListening(EventName.LoadData, OnLoadData);
+    }
+
+    protected abstract void OnSaveData(EventArg arg);
+    protected abstract void OnLoadData(EventArg arg);
+
+}

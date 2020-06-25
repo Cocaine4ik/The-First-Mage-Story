bcbdb176cfcfda0c881fe3ca9d0020fe066f45bb
Alex <35273835+Cocaine4ik@users.noreply.github.com>
2020-06-25 23:22:36 +0300

    Create SaveLoadPlayer.cs

---
+++ Assets/Scripts/SaveLoad/SaveLoadPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveLoadPlayer : SaveLoadData
+{
+    protected override void OnSaveData(EventArg arg) {
+
+        PlayerPrefs.SetFloat("PositionX", Attributes.Instance.transform.position.x);
+        PlayerPrefs.SetFloat("PositionY", Attributes.Instance.transform.position.y);
+
+        PlayerPrefs.SetInt("CurrentExp", Attributes.Instance.CurrentExp);
+        PlayerPrefs.SetInt("ExpToLevelUp", Attributes.Instance.ExpToLevelUp);
+        PlayerPrefs.SetInt("CurrentLevel", Attributes.Instance.CurrentLevel);
+    }
+
+    protected override void OnLoadData(EventArg arg) {
+        throw new System.NotImplementedException();
+    }
+}

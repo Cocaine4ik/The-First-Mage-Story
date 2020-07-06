bcb2d1707f29cc7446a89c6d8653555ef2214f6d
Cocaine <cocaine4ik13@gmail.com>
2020-07-06 07:30:24 +0300

    Update SaveTrigger.cs

--- Assets/Scripts/SaveLoad/SaveTrigger.cs
+++ Assets/Scripts/SaveLoad/SaveTrigger.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class SaveTrigger : MonoBehaviour
 {
+    [SerializeField] private bool withWind;
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
     public void SaveData() {
         EventManager.TriggerEvent(EventName.SaveData);
+        if(withWind)
+        {
+            animator.SetTrigger("BurnWithWind");
+        }
+        else
+        {
+            animator.SetTrigger("Burn");
+        }
         Debug.Log("SaveData");
     }
 }

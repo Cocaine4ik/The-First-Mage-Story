ab79633e0b9a4d76be95526b7f733362ce8fb478
Alex <35273835+Cocaine4ik@users.noreply.github.com>
2020-07-07 15:08:58 +0300

    Update TalkTrigger.cs

--- Assets/Scripts/Gameplay/Dialogues/TalkTrigger.cs
+++ Assets/Scripts/Gameplay/Dialogues/TalkTrigger.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 /// <summary>
 /// Abstract dialogue and speech trigger class
 /// </summary>
-public abstract class TalkTrigger : MonoBehaviour {
+public abstract class TalkTrigger : MonoBehaviour{
 
     [Header("Base Talk Options:")]
     [SerializeField] protected bool isInteractable = false;
     [SerializeField] protected bool isScriptable = false;
     [SerializeField] protected float scriptableTime;
+
+    protected bool dialogueSet = false;
 
     protected abstract void StartConversation();
 

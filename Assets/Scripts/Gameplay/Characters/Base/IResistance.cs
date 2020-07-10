b1c9c38a01789f2eb01b4bff62096e69dcf8be55
Alex <35273835+Cocaine4ik@users.noreply.github.com>
2020-07-10 17:37:15 +0300

    Create IResistance.cs

---
+++ Assets/Scripts/Gameplay/Characters/Base/IResistance.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IResistance
+{
+    int PhysicalResistance { get; }
+    int VeilResistannce { get; }
+    int FireResistance { get; }
+    int IceResistance { get; }
+    int NatureResistance { get; }
+    int DivineResitance { get; }
+}

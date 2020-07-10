6eff14c006e0106df21ebd77b4423d401eccb179
Alex <35273835+Cocaine4ik@users.noreply.github.com>
2020-07-10 17:37:04 +0300

    Update DamageData.cs

--- Assets/Scripts/Gameplay/Characters/Base/DamageData.cs
+++ Assets/Scripts/Gameplay/Characters/Base/DamageData.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DamageData : MonoBehaviour {
+public class DamageData : MonoBehaviour{
 
     [SerializeField] protected int damage;
     [SerializeField] protected float atackRange;

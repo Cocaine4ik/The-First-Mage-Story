f6ec019d319ea980b69c43dc0e2c65eb33477b18
Alex <35273835+Cocaine4ik@users.noreply.github.com>
2020-07-15 17:46:50 +0300

    Create ValleyOfTheWindsDirector.cs

---
+++ Assets/Scripts/Gameplay/Cut-scenes/GameLevelDirectors/ValleyOfTheWindsDirector.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValleyOfTheWindsDirector : SceneDirector
+{
+    [SerializeField] private GameObject spirit;
+    [SerializeField] private GameObject archmage;
+    [SerializeField] private float timeBeforeAchmageMove = 2f;
+
+    private SpeechTrigger spiritSpeechTrigger;
+    private CharacterHealth spiritHealth;
+
+    private MageBehaviour mageBehaviour;
+
+    private void Start() {
+
+        spiritSpeechTrigger = spirit.GetComponent<SpeechTrigger>();
+        spiritHealth = spirit.GetComponent<CharacterHealth>();
+        mageBehaviour = archmage.GetComponent<MageBehaviour>();
+        mageBehaviour.enabled = false;
+        StartCoroutine(StartArchmageMovement(timeBeforeAchmageMove));
+    }
+
+    private void Update() {
+        
+        if(spiritSpeechTrigger.SpeechIsEnd == true && spiritHealth != null) {
+            spiritHealth.TakeDamage(10);
+            StartCoroutine(GoToNextScene(SceneName.MagicCliffs));
+        }
+    }
+
+    private IEnumerator StartArchmageMovement(float waitTime) {
+        yield return new WaitForSeconds(waitTime);
+        mageBehaviour.enabled = true;
+    }
+}

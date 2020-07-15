cf5c41e933f433e0590d18aecc13bb7aefd29ca0
Alex <35273835+Cocaine4ik@users.noreply.github.com>
2020-07-15 17:46:38 +0300

    Create MagicCliffsDirector.cs

---
+++ Assets/Scripts/Gameplay/Cut-scenes/GameLevelDirectors/MagicCliffsDirector.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicCliffsDirector : MonoBehaviour
+{
+    [SerializeField] private float alphaPerFrame;
+    [SerializeField] private GameObject movingPlatform;
+    [SerializeField] private GameObject player;
+    private SpriteRenderer playerSprite;
+    private bool appearFlag = false;
+
+    void Start()
+    {
+        playerSprite = player.GetComponent<SpriteRenderer>();
+        EventManager.StartListening(EventName.AppearPlayer, OnAppearPlayer);
+        EventManager.StartListening(EventName.PickupItem, OnPlatformUp);
+    }
+    private void Update()
+    {
+        if (appearFlag == true)
+        {
+            AppearPlayer(alphaPerFrame);
+        }
+    }
+    private void OnDisable()
+    {
+        EventManager.StopListening(EventName.AppearPlayer, OnAppearPlayer);
+    }
+
+    private void OnAppearPlayer(EventArg arg)
+    {
+        appearFlag = true;
+    }
+    private void OnPlatformUp(EventArg arg)
+    {
+        if (arg.Item.name == "FlyingStones")
+        {
+            movingPlatform.GetComponent<MovingPlatform>().MoveUp();
+            AudioManager.SFXAudioSource.Play(SFXClipName.PlatformMove);
+            EventManager.StopListening(EventName.PickupItem, OnPlatformUp);
+        }
+    }
+    private void AppearPlayer(float alphaPerFrame)
+    {
+
+        if (playerSprite != null)
+        {
+
+            var tempColor = playerSprite.color;
+            tempColor.a += alphaPerFrame;
+            playerSprite.color = tempColor;
+
+            if (playerSprite.color.a > 1)
+            {
+                appearFlag = false;
+                StatusUtils.CutScenePlaying = false;
+            }
+
+        }
+    }
+}

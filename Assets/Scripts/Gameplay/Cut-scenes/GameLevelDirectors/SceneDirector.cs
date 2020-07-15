6a6820ffbed8371cc86556297e523af044756b58
Alex <35273835+Cocaine4ik@users.noreply.github.com>
2020-07-15 17:46:43 +0300

    Create SceneDirector.cs

---
+++ Assets/Scripts/Gameplay/Cut-scenes/GameLevelDirectors/SceneDirector.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Scene director
+/// </summary>
+public class SceneDirector : MonoBehaviour
+{
+    
+    protected IEnumerator GoToNextScene(SceneName name)
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(name.ToString());
+    }
+}

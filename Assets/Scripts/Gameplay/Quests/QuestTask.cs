911fac21eae8d4a427b14c86b81014b11c06b010
Cocaine <cocaine4ik13@gmail.com>
2020-08-13 17:03:29 +0300

    Update QuestTask.cs

--- Assets/Scripts/Gameplay/Quests/QuestTask.cs
+++ Assets/Scripts/Gameplay/Quests/QuestTask.cs
@@ -14,11 +14,18 @@ public class QuestTask : ScriptableObject, IJournalItem
     [SerializeField] private Item itemReward;
 
     [Header("Collect Task Options")]
-    [SerializeField] private QuestItem itemToCollect;
+    [SerializeField] private ItemName itemToCollect;
     [SerializeField] private int collectItemNumber;
+    private int collectedItemNumber = 0;
 
     public CompletnessStatus Status { get => status; set => status = value; }
+    public TaskType TaskType => taskType;
     public int Id => id;
     public string NameKey => "Quests.Tasks." + questName.ToString() + "-" + id;
     public string DescriptionKey => "Quests.Description." + questName.ToString() + "-" + id;
+
+    public void RefreshCollectTask()
+    {
+
+    }
 }

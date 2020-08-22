95ee87a6c1885d1a08d66bec5caa04596b2ffcc1
Cocaine <cocaine4ik13@gmail.com>
2020-08-22 15:18:34 +0300

    Update SaveLoadJournal.cs

--- Assets/Scripts/SaveLoad/SaveLoadJournal.cs
+++ Assets/Scripts/SaveLoad/SaveLoadJournal.cs
@@ -1,18 +1,56 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SaveLoadJournal : MonoBehaviour
+public class SaveLoadJournal : SaveLoadData
 {
-    // Start is called before the first frame update
-    void Start()
+    private QuestJournal questJournal;
+
+    private void Awake()
     {
-        
+        questJournal = GetComponent<QuestJournal>();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void OnLoadData(EventArg arg)
     {
-        
+        foreach (QuestName name in Enum.GetValues(typeof(QuestName)))
+        {
+            if (PlayerPrefs.HasKey("QuestData." + name.ToString()))
+            {
+                var questData = PlayerPrefs.GetString("QuestData." + name.ToString());
+                var splitQuestData = questData.Split('.');
+                var questName = (QuestName)Enum.Parse(typeof(QuestName), splitQuestData[0]);
+                var questStatus = (CompletnessStatus)Enum.Parse(typeof(CompletnessStatus), splitQuestData[1]);
+                var currentTaskId = Int16.Parse(splitQuestData[2]);
+
+                QuestSystem.Instance.AddQuest(questName);
+                var quest = QuestSystem.Instance.Quests[questName];
+                foreach (QuestTask task in QuestSystem.Instance.Quests[questName].QuestTasks)
+                {
+                    if (task.Id != currentTaskId)
+                    {
+                        task.Status = CompletnessStatus.Done;
+                        questJournal.CloseTask(task);
+                        QuestSystem.Instance.AddTask(quest);
+                        questJournal.AddTaskToJournal(quest);
+                    }
+                }
+                if (questStatus == CompletnessStatus.Done || questStatus == CompletnessStatus.Failed)
+                {
+                    questJournal.CloseTask(quest.CurrentTask);
+                    quest.Status = questStatus;
+                }
+            }
+        }
+    }
+
+    protected override void OnSaveData(EventArg arg)
+    {
+        foreach(var quest in QuestSystem.Instance.Quests)
+        {
+            PlayerPrefs.SetString("QuestData." + quest.Key.ToString(), quest.Key.ToString() +
+                "." + quest.Value.Status.ToString() + "." + quest.Value.CurrentTask.Id.ToString());
+        }
     }
 }

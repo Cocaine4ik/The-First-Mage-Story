3fee3ad811d0122437779876231f35001ff009de
Alex <35273835+Cocaine4ik@users.noreply.github.com>
2020-09-08 17:06:54 +0300

    Update DialogueSystem.cs

--- Assets/Scripts/Gameplay/Dialogues/DialogueSystem.cs
+++ Assets/Scripts/Gameplay/Dialogues/DialogueSystem.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+<<<<<<< Updated upstream
 using UnityEngine;
 
 public class DialogueSystem : Singleton<DialogueSystem>
+=======
+using System.Linq;
+using UnityEngine;
+
+public class DialogueSystem : MonoBehaviour
+>>>>>>> Stashed changes
 {
     private DialogueWindow dialogueWindow;
     private DialogueParser dialogueParser;
 
+<<<<<<< Updated upstream
     private void Start()
     {
         dialogueWindow = GetComponent<DialogueWindow>();
@@ -15,4 +23,36 @@ public class DialogueSystem : Singleton<DialogueSystem>
 
 
 
+=======
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueWindow = GetComponent<DialogueWindow>();
+        dialogueParser = GetComponent<DialogueParser>();
+        
+    }
+
+    public void StartConversation(DialogueContainer dialogue, Sprite leftPortait, Sprite rightPortrait, string leftSpeakerKey, string rightSpeakerKey)
+    {
+        SetSpeakers(leftPortait, rightPortrait, leftSpeakerKey, rightSpeakerKey);
+        dialogueParser.SetDialogue(dialogue);
+        if(dialogueParser.Dialogue != null)
+        {
+            var dialogueData = dialogueParser.Dialogue.NodeLinks.First();
+            dialogueParser.ProceedToDialogue(dialogueData.TargetNodeGUID);
+        }
+        EventManager.TriggerEvent(EventName.StartConversation);
+    }
+
+    public void StartScriptableConversation(DialogueContainer dialogue, Sprite leftPortait, Sprite rightPortrait, string leftSpeakerKey, string rightSpeakerKey)
+    {
+        StartConversation(dialogue, leftPortait, rightPortrait, leftSpeakerKey, rightSpeakerKey);
+    }
+
+    public void SetSpeakers(Sprite leftPortait, Sprite rightPortrait, string leftSpeakerKey, string rightSpeakerKey)
+    {
+        dialogueWindow.SetLeftSpeaker(leftPortait, leftSpeakerKey);
+        dialogueWindow.SetRightSpeaker(rightPortrait, rightSpeakerKey);
+    }
+>>>>>>> Stashed changes
 }
